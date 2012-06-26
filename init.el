c3507f066105fff936038e2c7efbb11f96ec5b4a
Maris <maris@ubuntu.dlink.com>
2012-06-26 13:45:41 +0100

    code fix

--- init.el
+++ init.el
@@ -91,9 +91,9 @@
 ;; customizations
 (menu-bar-mode -1) 
 (show-paren-mode t)
-(if 
-    (boundp 'tool-bar-mode)
+(if (and window-system (boundp 'tool-bar-mode))
     (tool-bar-mode -1))
+
 (if 
     (boundp 'scroll-bar-mode)
     (scroll-bar-mode -1))
