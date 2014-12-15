500b6c8cf993f9073b6fc66e50902f4ed1545217
Maris Orbidans <MarisOrbidans@thisisnoble.com>
2014-12-15 01:07:53 +0000

    added backup script

---
+++ dot-files/backup-emacs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FILENAME=emacs-$(date +%F-%H-%M).tar.xz
+
+cd
+
+tar -c --xz -f /tmp/$FILENAME .emacs.d
+
+scp /tmp/$FILENAME maruks.homelinux.org:/mnt/internal/Backups
+
+rm /tmp/$FILENAME
