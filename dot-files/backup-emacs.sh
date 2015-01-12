c9697c3bb2b30003fcad6044d5c1de1d3d3840a3
Maris Orbidans <MarisOrbidans@thisisnoble.com>
2015-01-12 18:46:58 +0000

    script update

--- dot-files/backup-emacs.sh
+++ dot-files/backup-emacs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-FILENAME=emacs-$(date +%F-%H-%M).tar.xz
+FILENAME=emacs-$(hostname)-$(date +%F-%H-%M).tar.xz
 
 cd
 
-tar -c --xz -f /tmp/$FILENAME .emacs.d
+tar -c --xz -f "/tmp/$FILENAME" .emacs.d
 
-scp /tmp/$FILENAME maruks.homelinux.org:/mnt/internal/Backups
+scp "/tmp/$FILENAME" maruks.homelinux.org:/mnt/internal/Backups
 
-rm /tmp/$FILENAME
+rm "/tmp/$FILENAME"
