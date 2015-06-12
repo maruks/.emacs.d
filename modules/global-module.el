ab0bf920846e785d09bf2a7e623c9dd88a86229b
Maris Orbidans <maris.orbidans@agentsmutual.co.uk>
2015-06-12 18:42:52 +0100

    projectile keys

--- modules/global-module.el
+++ modules/global-module.el
@@ -91,6 +91,11 @@
 (require 'projectile)
 (projectile-global-mode)
 
+(define-key projectile-mode-map [?\s-d] 'projectile-find-dir)
+(define-key projectile-mode-map [?\s-p] 'projectile-switch-project)
+(define-key projectile-mode-map [?\s-f] 'projectile-find-file)
+(define-key projectile-mode-map [?\s-g] 'projectile-grep)
+
 ;; global keys
 
 (global-set-key (kbd "\e\eg") 'goto-line)
