b820f3b1f63b156cf966bc223928ece9b64ef4ac
Maris Orbidans <MarisOrbidans@thisisnoble.com>
2015-07-02 01:11:36 +0100

    profiles update

--- dot-files/profiles.clj
+++ dot-files/profiles.clj
@@ -1,57 +1,41 @@
 {:user
- {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
-            [refactor-nrepl "1.1.0-SNAPSHOT"]
-            [lein-exec "0.3.4"]]
+ {:plugins [[cider/cider-nrepl "0.9.1"]
+            [speclj "3.3.0"]
+            [refactor-nrepl "1.0.5"]
+            [lein-exec "0.3.5"]]
 
-  :dependencies [[im.chit/iroh "0.1.11"]
-                 [im.chit/vinyasa "0.2.2"]
-                 [leiningen "2.5.0"]
-                 [spyscope "0.1.4"]
-                 [difform "1.1.2"]
-                 [criterium "0.4.3"]
-                 [org.clojars.gjahad/debug-repl "0.3.3"]
-                 [io.aviso/pretty "0.1.8"]
-                 [org.clojure/tools.namespace "0.2.4"]
-                 [org.clojure/tools.nrepl "0.2.7"]]
+  :dependencies [[leiningen #=(leiningen.core.main/leiningen-version)]
+                 [im.chit/vinyasa "0.3.4"]
+                 [spyscope "0.1.5"]
+                 [io.aviso/pretty "0.1.18"]
+                 [org.clojure/tools.namespace "0.2.11"]
+                 [org.clojure/tools.nrepl "0.2.10"]]
 
   :signing {:gpg-key "D4FF9261"}        ; clojars signing key
 
 ;; Reflection macros: .> .? .* .% .%>
-
-;; criterium
-;; (bench (Thread/sleep 1000) :verbose)
-;; (with-progress-reporting (bench (Thread/sleep 1000) :verbose))
-;; (report-result (benchmark (Thread/sleep 1000)) :verbose)
-;; (report-result (quick-bench (Thread/sleep 1000)))
 
 ;; spyscope
 ;; #spy/p #spy/d #spy/t
 
   :injections [(require '[vinyasa.inject :as inject]
                         'spyscope.core
-                        'criterium.core
-                        'alex-and-georges.debug-repl
-                        'com.georgejahad.difform
                         'io.aviso.repl)
 
                (inject/in
 
                 ;; the default injected namespace is `.`
+                [vinyasa.inject :refer [inject [in inject-in]]]
+                [vinyasa.lein :exclude [*project*]]
 
-                [vinyasa.inject inject]
                 [vinyasa.pull pull]
-                [vinyasa.lein lein]
-                [vinyasa.reimport reimport]
-                [criterium.core with-progress-reporting report-result bench quick-bench benchmark]
-
-                clojure.core
-                [iroh.core .> .? .* .% .%>]
-                [alex-and-georges.debug-repl debug-repl]
-
-                clojure.core >
                 [cemerick.pomegranate add-classpath get-classpath resources]
+                [clojure.pprint pprint]
+                [clojure.java.shell sh]
                 [clojure.tools.namespace.repl refresh]
                 [clojure.repl apropos dir doc find-doc source pst [root-cause >cause]]
                 [clojure.pprint pprint]
-                [com.georgejahad.difform difform]
-                [clojure.java.shell sh])]}}
+
+                ;; inject into clojure.core
+                clojure.core
+                [vinyasa.reflection .> .? .* .% .%> .& .>ns .>var])]}}
