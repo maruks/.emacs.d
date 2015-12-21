7af9d0c1f286ca20f964035164cfc0a8c6de0d38
Maris Orbidans <MarisOrbidans@thisisnoble.com>
2015-12-21 01:07:33 +0000

    code fix

--- modules/haskell-module.el
+++ modules/haskell-module.el
@@ -16,6 +16,7 @@
 
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'hindent-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
 (add-hook 'haskell-mode-hook 'company-mode)
 
