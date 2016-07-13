1419d40fc4c13786b38d44be6c218d218205ee62
Maris Orbidans <maris.orbidans@gmail.com>
2016-07-13 23:20:24 +0100

    haskell mode fix

--- modules/haskell-module.el
+++ modules/haskell-module.el
@@ -48,7 +48,8 @@
 				  (define-key haskell-mode-map (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
 				  (define-key haskell-mode-map (kbd "C-c C-n c") 'haskell-process-cabal)
 				  (define-key haskell-mode-map (kbd "C-c C-o") 'haskell-compile)
-				  (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)))
+				  ;(define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
+				  ))
 
 (eval-after-load 'haskell-cabal '(progn
 				   (define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
