3698ea0ffe98100acf8e0758efe4458da21e0b4c
Maris Orbidans <maris.orbidans@agentsmutual.co.uk>
2017-01-21 23:26:44 +0000

    idris flatland

--- modules/idris-module.el
+++ modules/idris-module.el
@@ -26,6 +26,29 @@
    (idris-antiquotation-face ((t (:background "white" :box "light gray")))))
  idris-font-lock-table)
 
+(puthash
+ 'flatland
+ '((idris-semantic-type-face ((t (:inherit font-lock-type-face))))
+   (idris-keyword-face ((t (:inherit font-lock-keyword-face))))
+   (idris-semantic-bound-face ((t (:inherit font-lock-variable-name-face))))
+   (idris-semantic-function-face ((t (:foreground "#f8f8f8"))))
+   (idris-identifier-face ((t (:foreground "#93e0e3"))))
+   (idris-equals-face ((t (:foreground "#e0e0e0"))))
+   (idris-colon-face ((t (:foreground "#e0e0e0" ))))
+   (idris-hole-face ((t (:underline (:color "#ff3a83" :style wave) :slant italic))))
+   (idris-operator-face ((t (:foreground "#dc8cc3"))))
+   (idris-repl-output-face ((t (:foreground "#afc4db"))))
+   (idris-semantic-implicit-face ((t (:slant italic))))
+   (idris-semantic-data-face ((t (:foreground "#cfe2f2"))))
+   (idris-directive-face ((t (:foreground "#b1b3ba"))))
+   (idris-inline-doc-face ((t (:foreground "#b1b3ba"))))
+   (idris-info-title-face ((t (:height 1.8 :weight bold :foreground "#72aaca" :family "PT Sans Narrow"))))
+   (idris-quasiquotation-face ((t (:background "#b0b3ba"))))
+   (idris-antiquotation-face ((t (:background "#73817d" :box "#b0b3ba"))))
+   (idris-parameter-face ((t (:inherit idris-semantic-bound-face))))
+   (idris-module-face ((t (:inherit idris-semantic-module-face)))))
+ idris-font-lock-table)
+
 (defun idris-apply-font-locks (theme)
   (let ((faces (gethash theme idris-font-lock-table)))
     (when faces
