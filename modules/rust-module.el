581c5845930a70fcf6b97d2c339a92cfbcde55fb
Maris Orbidans <maris.orbidans@agentsmutual.co.uk>
2017-06-11 21:34:42 +0100

    rust

--- modules/rust-module.el
+++ modules/rust-module.el
@@ -2,17 +2,22 @@
 
 (require 'package-module)
 
+(package-require 'company)
 (package-require 'cargo)
+(package-require 'flycheck)
 (package-require 'flycheck-rust)
 (package-require 'rust-mode)
 (package-require 'racer)
 (package-require 'rust-playground)
 
 (add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'rust-mode-hook 'flycheck-mode)
 (add-hook 'rust-mode-hook 'cargo-minor-mode)
 
 (add-hook 'racer-mode-hook #'eldoc-mode)
 (add-hook 'racer-mode-hook #'company-mode)
+
+(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
 
 (require 'rust-mode)
 (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
