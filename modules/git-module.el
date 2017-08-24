015858cc57d69215dd193c3cc3ed26a62610dc5b
Maris Orbidans <maris.orbidans@agentsmutual.co.uk>
2017-08-24 19:02:17 +0100

    add git timemachine

--- modules/git-module.el
+++ modules/git-module.el
@@ -4,8 +4,13 @@
 
 ;; magit
 (package-require 'magit)
+(package-require 'git-timemachine)
+(package-require 'gitconfig-mode)
 
 (global-set-key (kbd "\e\em") 'magit-status)
 (global-set-key (kbd "<f8>") 'magit-status)
+
+(global-set-key (kbd "\e\et") 'git-timemachine)
+(global-set-key (kbd "\e\eb") 'magit-blame)
 
 (provide 'git-module)
