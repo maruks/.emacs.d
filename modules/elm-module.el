5294831f4bf98d988abf408b793d7c1e400b039a
Maris Orbidans <MarisOrbidans@thisisnoble.com>
2017-10-23 23:59:03 +0100

    elm mode fixes

--- modules/elm-module.el
+++ modules/elm-module.el
@@ -1,11 +1,15 @@
 ;; elm-module
+;; https://github.com/jcollard/elm-mode
+;; npm install --global elm elm-live elm-test elm-format@exp elm-oracle
 
 (require 'package-module)
 
 (package-require 'elm-mode)
+(package-require 'company)
 
-(add-hook 'elm-mode-hook #'elm-oracle-setup-completion)
+(add-hook 'elm-mode-hook #'company-mode)
 
-;; https://github.com/jcollard/elm-mode
+(with-eval-after-load "company"
+  (add-to-list 'company-backends 'company-elm))
 
 (provide 'elm-module)
