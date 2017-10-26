6f41e37d7ce588bc97b2b930f2d18b4b26fb3837
Maris Orbidans <maris.orbidans@agentsmutual.co.uk>
2017-10-27 00:52:13 +0100

    add flycheck elm

--- modules/elm-module.el
+++ modules/elm-module.el
@@ -5,11 +5,25 @@
 (require 'package-module)
 
 (package-require 'elm-mode)
+(package-require 'flycheck-elm)
 (package-require 'company)
+
+(add-hook 'elm-mode-hook 'flycheck-mode)
+(add-hook 'flycheck-mode-hook #'flycheck-elm-setup)
 
 (add-hook 'elm-mode-hook #'company-mode)
 
 (with-eval-after-load "company"
   (add-to-list 'company-backends 'company-elm))
+
+
+(defun save-buffer-if-modified (&rest args)
+  (when (and buffer-file-name (buffer-modified-p)) (save-buffer)))
+
+(eval-after-load 'elm-mode
+  '(progn
+     (define-key elm-mode-map [?\s-c] 'elm-compile-buffer)
+     (define-key elm-mode-map (kbd "<f5>") 'elm-compile-buffer)
+     (advice-add 'elm-compile-buffer :before  #'save-buffer-if-modified)))
 
 (provide 'elm-module)
