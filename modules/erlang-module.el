ec9714b2fd0f9dfe089dfc523a2717c3174845b9
Maris Orbidans <MarisOrbidans@thisisnoble.com>
2017-11-16 01:53:58 +0000

    erlang mode hack

--- modules/erlang-module.el
+++ modules/erlang-module.el
@@ -7,12 +7,6 @@
 
 (flycheck-rebar3-setup)
 
-(defun use-rebar3-shell ()
-  (interactive)
-  (setq inferior-erlang-machine "rebar3")
-  (setq inferior-erlang-machine-options '("shell"))
-  (setq inferior-erlang-shell-type nil))
-
 (defun load-erlang ()
   (add-to-list 'load-path "~/.emacs.d/vendor/erlang")
   (let ((erlang-root (or (getenv "_KERL_ACTIVE_DIR") "/usr/local/lib/erlang")))
@@ -57,6 +51,20 @@
 ;; C-c C-e c
 (add-hook 'erlang-mode-hook
 	  (lambda ()
+	    (flycheck-mode)))
+
+(defun start-shell-in-project-root (orig-fun &rest args)
+  (let ((default-directory (flycheck-rebar3-project-root)))
+    (apply orig-fun args)))
+
+(defun use-rebar-shell ()
+  (interactive)
+  (advice-add 'erlang-shell-display :around #'start-shell-in-project-root)
+  (setq inferior-erlang-machine "rebar3")
+  (setq inferior-erlang-machine-options '("shell"))
+  (setq inferior-erlang-shell-type nil))
+
+(with-eval-after-load 'erlang
 	    (define-key erlang-mode-map (kbd "<f5>") 'erlang-compile)
 	    (define-key erlang-mode-map [?\s-c] 'erlang-compile)
             (define-key erlang-mode-map (kbd "<f6>") 'erlang-eunit-compile-and-run-module-tests)
@@ -65,9 +73,7 @@
 	    (setq erlang-compile-extra-opts '((i . \"../include\") (d . \'TEST\')))
 	    (advice-add 'erlang-compile :before #'save-current-buffer-if-modified)
 	    (advice-add 'erlang-eunit-compile-and-run-module-tests :before #'save-current-buffer-if-modified)
-	    (flycheck-mode)))
-
-(eval-after-load 'erlang
-  '(message "Erlang root is %s." erlang-root-dir))
+	    (use-rebar-shell)
+	    (message "Erlang root is %s." erlang-root-dir))
 
 (provide 'erlang-module)
