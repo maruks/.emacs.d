d4b1dd146cfeea1b33c3c209909a3b19365d492b
Maris Orbidans <maris.orbidans@agentsmutual.co.uk>
2018-01-18 21:08:37 +0000

    git timemachine

--- modules/git-module.el
+++ modules/git-module.el
@@ -10,7 +10,8 @@
 (global-set-key (kbd "\e\em") 'magit-status)
 (global-set-key (kbd "<f8>") 'magit-status)
 
-(global-set-key (kbd "\e\et") 'git-timemachine)
+(global-set-key (kbd "M-g t") 'git-timemachine-toggle)
+(global-set-key (kbd "\e\et") 'git-timemachine-toggle)
 (global-set-key (kbd "\e\eb") 'magit-blame)
 
 (provide 'git-module)
