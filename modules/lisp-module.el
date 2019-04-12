66c7262293bb14b3cd17950a742cbba9cfc2e984
Maris Orbidans <maris.orbidans@onthemarket.com>
2019-04-12 17:32:24 +0100

    install sly

--- modules/lisp-module.el
+++ modules/lisp-module.el
@@ -2,18 +2,13 @@
 
 (require 'package-module)
 
-(package-require 'slime)
-(package-require 'slime-company)
+(package-require 'sly)
 
 (setq inferior-lisp-program "sbcl")
 
 (eval-after-load 'lisp-mode
   '(progn
-     (slime-setup '(slime-fancy slime-asdf slime-sbcl-exts slime-company))
-     (define-key lisp-mode-map [?\s-c] 'slime-compile-and-load-file)
-     (define-key lisp-mode-map (kbd "<f10>") 'slime-compile-and-load-file)
-     (define-key lisp-mode-map (kbd "<f11>") 'slime)
-     (define-key lisp-mode-map (kbd "<f12>") 'slime-selector)
-     (advice-add 'slime-compile-and-load-file :before #'save-current-buffer-if-modified)))
+     (define-key lisp-mode-map [?\s-c] 'sly-compile-and-load-file)
+     (advice-add 'sly-compile-and-load-file :before #'save-current-buffer-if-modified)))
 
 (provide 'lisp-module)
---
+++ modules/lisp-slime-module.el
@@ -0,0 +1,19 @@
+;; lisp-slime-module
+
+(require 'package-module)
+
+(package-require 'slime)
+(package-require 'slime-company)
+
+(setq inferior-lisp-program "sbcl")
+
+(eval-after-load 'lisp-mode
+  '(progn
+     (slime-setup '(slime-fancy slime-asdf slime-sbcl-exts slime-company))
+     (define-key lisp-mode-map [?\s-c] 'slime-compile-and-load-file)
+     (define-key lisp-mode-map (kbd "<f10>") 'slime-compile-and-load-file)
+     (define-key lisp-mode-map (kbd "<f11>") 'slime)
+     (define-key lisp-mode-map (kbd "<f12>") 'slime-selector)
+     (advice-add 'slime-compile-and-load-file :before #'save-current-buffer-if-modified)))
+
+(provide 'lisp-slime-module)
