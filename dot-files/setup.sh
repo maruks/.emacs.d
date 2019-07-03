59ac5ff966102e3e58d3ec3656e51ec790466987
Maris Orbidans <maris.orbidans@onthemarket.com>
2019-07-03 12:37:32 +0100

    update few files

--- dot-files/setup.sh
+++ dot-files/setup.sh
@@ -12,6 +12,7 @@ mkdir -p ~/.lein
 mkdir -p ~/.ssh
 
 cp ~/.emacs.d/dot-files/.zshrc ~
+cp ~/.emacs.d/dot-files/zshenv.home ~/.zshenv
 cp ~/.emacs.d/dot-files/.gitconfig ~
 cp ~/.emacs.d/dot-files/profiles.clj ~/.lein
 cp ~/.emacs.d/dot-files/config ~/.ssh
