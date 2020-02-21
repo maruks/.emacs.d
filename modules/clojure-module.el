d59197443c7fbe7e96377319668c17a421f4ae22
Maris Orbidans <maris.orbidans@onthemarket.com>
2020-02-21 14:20:22 +0000

    enable cljr mode

--- modules/clojure-module.el
+++ modules/clojure-module.el
@@ -5,11 +5,11 @@
 (package-require 'clojure-mode)
 (package-require 'clojure-mode-extra-font-locking)
 (package-require 'cider)
-;; (package-require 'clj-refactor)
+(package-require 'clj-refactor)
 (package-require 'align-cljlet)
 
 (require 'clojure-mode)
-;; (require 'clj-refactor)
+(require 'clj-refactor)
 
 (defun clj-insert-fn ()
   (interactive)
@@ -31,10 +31,10 @@
      (advice-add 'cider-load-buffer :before #'save-current-buffer-if-modified)))
 
 ;; clj-refactor
-;; (add-hook 'clojure-mode-hook
-;; 	  (lambda ()
-;; 	    (clj-refactor-mode 1)
-;; 	    (cljr-add-keybindings-with-prefix "C-c C-r")))
+(add-hook 'clojure-mode-hook
+	  (lambda ()
+	    (clj-refactor-mode 1)
+	    (cljr-add-keybindings-with-prefix "C-c C-r")))
 
 ;; cider
 (add-hook 'cider-mode-hook #'eldoc-mode)
