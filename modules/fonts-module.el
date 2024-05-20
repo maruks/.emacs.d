fd6025a73e76a5c90d6a9e015e0010d8c60772de
Maris Orbidans <maris.orbidans@agentsmutual.co.uk>
2024-05-20 17:44:00 +0100

    add fonts

--- modules/fonts-module.el
+++ modules/fonts-module.el
@@ -14,4 +14,22 @@
   (set-frame-font font nil t)
   (add-to-list 'default-frame-alist (cons 'font font)))
 
+(setq font-presets
+      '((mono-lisa .       "MonoLisa 2:pixelsize=23:weight=regular:slant=normal:width=normal:spacing=100:scalable=true")
+	(mono-lisa-light . "MonoLisa 2:pixelsize=23:weight=light:slant=normal:width=normal:spacing=100:scalable=true")
+        (fira-code .       "Fira Code:pixelsize=24:weight=regular:slant=normal:width=normal:spacing=100:scalable=true")
+        (fira-code-light . "Fira Code:pixelsize=24:weight=light:slant=normal:width=normal:spacing=100:scalable=true")
+        (berkeley-mono .   "Berkeley Mono:pixelsize=24:weight=regular:slant=normal:width=normal:spacing=100:scalable=true")
+        (jetbrains-mono .  "JetBrains Mono:pixelsize=24:weight=regular:slant=normal:width=normal:spacing=100:scalable=true")
+        (jetbrains-mono-light . "JetBrains Mono:pixelsize=24:weight=light:slant=normal:width=normal:spacing=100:scalable=true")))
+
+
+(defun set-f0nt (preset)
+  (interactive
+   (let ((completion-ignore-case  t))
+     (list (completing-read "Choose: " (mapcar 'car font-presets) nil t))))
+  (let ((font (cdr (assoc (intern preset) font-presets))))
+    (set-frame-font font)
+    (message "Selected font: `%s'" font)))
+
 (provide 'fonts-module)
